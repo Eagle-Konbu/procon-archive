bc51b81d91c95da78093f1cb2db438325de9e222
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-08-21 12:00:55 +0000

    [AC] abc215 abc215_a

---
+++ atcoder.jp/abc215/abc215_a/Main.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        s:String
+    };
+
+    if s == "Hello,World!".to_string() {
+        println!("AC");
+    } else {
+        println!("WA");
+    }
+}
