41eb5600643e3c96e6f9f601a2a8f6da7fcbc821
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-09-01 00:48:19 +0000

    [AC] abc045 abc045_a

---
+++ atcoder.jp/abc045/abc045_a/Main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
+
+int main()
+{
+    int a, b, h;
+    cin >> a >> b >> h;
+
+    cout << (a + b) * h / 2 << endl;
+}
