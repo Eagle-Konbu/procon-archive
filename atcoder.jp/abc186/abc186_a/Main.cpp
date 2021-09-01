98288317ab24404e3a6c0de2e529eec1fd25e0ba
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-09-01 00:56:24 +0000

    [AC] abc186 abc186_a

---
+++ atcoder.jp/abc186/abc186_a/Main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
+
+int main()
+{
+    int n, w;
+    cin >> n >> w;
+
+    cout << n / w << endl;
+}
