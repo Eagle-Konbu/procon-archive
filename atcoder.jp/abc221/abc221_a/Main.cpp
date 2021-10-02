1d29456ca52424d8dd1fed9a3ffa577f4a927a09
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-10-02 12:04:17 +0000

    [AC] abc221 abc221_a

---
+++ atcoder.jp/abc221/abc221_a/Main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1;; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = ((ll)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (ll i = ((ll)(n)); i > 0; i--)
+#define ALL(x) (x).begin(), (x).end()
+#define pb push_back
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int diff = a - b;
+    ll ans = 1;
+
+    rep(i,diff) ans *= 32;
+
+    cout << ans << endl;
+
+    return 0;
+}
