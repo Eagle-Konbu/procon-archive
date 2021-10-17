1dc5ce9a19397e0f4d81daf08bbdd474434f50ac
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-10-17 12:01:19 +0000

    [AC] abc223 abc223_a

---
+++ atcoder.jp/abc223/abc223_a/Main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1;; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = ((ll)(n)-1); i >= 0; i--)
+#define rreps(i, n) for (ll i = ((ll)(n)); i > 0; i--)
+#define ALL(x) (x).begin(), (x).end()
+#define pb push_back
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    if (x % 100 == 0 && x != 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
