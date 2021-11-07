c488084a8b4d6651fa0f7b5ea5fbb3d73de5dd2c
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2021-11-07 12:01:57 +0000

    [AC] abc226 abc226_a

---
+++ atcoder.jp/abc226/abc226_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define ALL(x) (x).begin, (x).end
+#define pb push_back
+
+int main()
+{
+    double x;
+    cin >> x;
+
+    cout << round(x) << endl;
+
+    return 0;
+}
