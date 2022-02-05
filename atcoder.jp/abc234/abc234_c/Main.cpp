99119508b81d5ca2eb27f25952e605058e8fa900
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-05 06:31:53 +0000

    [AC] abc234 abc234_c

---
+++ atcoder.jp/abc234/abc234_c/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    ll k;
+    cin >> k;
+    string s;
+
+    while(k>0) {
+        if(k%2==0) s.push_back('0');
+        else s.push_back('2');
+
+        k /= 2;
+    }
+    reverse(s.begin(), s.end());
+
+    cout << s << endl;
+    return 0;
+}
