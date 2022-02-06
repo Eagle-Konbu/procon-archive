b2c5572b1a7f85459568e49b78c66b59688eadae
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-06 06:22:22 +0000

    [AC] abc206 abc206_d

---
+++ atcoder.jp/abc206/abc206_d/Main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    rep(i, n) cin >> a[i];
+
+    const int m = 2e5+5;
+    dsu d(m);
+    rep(i, n) d.merge(a[i], a[n - i - 1]);
+
+    int ans = 0;
+    rep(i, m)
+    {
+        if (d.leader(i) != i)
+            continue;
+
+        ans += d.size(i) - 1;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
