7dc2f00ad9fafe5b490857fb6388b2e227851bbc
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-06 20:50:24 +0000

    [AC] abc233 abc233_d

---
+++ atcoder.jp/abc233/abc233_d/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+
+    vector<ll> sum(n + 1, 0);
+    rep(i, n) sum[i + 1] = a[i] + sum[i];
+
+    map<ll, ll> mp;
+    ll ans = 0;
+    rep(i, n + 1)
+    {
+        ans += mp[sum[i]];
+        mp[sum[i] + k]++;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
