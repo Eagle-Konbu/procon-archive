99e800f93c4b71a6e820113661a27b5b1498ba20
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-19 12:21:23 +0000

    [AC] abc239 abc239_c

---
+++ atcoder.jp/abc239/abc239_c/Main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    ll x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    vector<P> points;
+
+    for (int i = -2; i <= 2; i++)
+    {
+        for (int j = -2; j <= 2; j++)
+        {
+            ll x = x1 + i, y = y1 + j;
+            if (pow(x1 - x, 2) + pow(y1 - y, 2) == 5)
+            {
+                points.push_back({x, y});
+            }
+        }
+    }
+
+    for (auto p : points)
+    {
+        if (pow(x2 - p.first, 2) + pow(y2 - p.second, 2) == 5)
+        {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+
+    return 0;
+}
