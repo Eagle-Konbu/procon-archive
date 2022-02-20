4112a557992dc971a7b9fbe8684ada79b98cbef0
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-20 12:02:16 +0000

    [AC] abc240 abc240_a

---
+++ atcoder.jp/abc240/abc240_a/Main.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (b - a == 1 || a - b == 1)
+    {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    if (b == 10 && a == 1)
+    {
+        cout << "Yes" << endl;
+        return 0;
+    }
+    if (a == 10 && b == 1)
+    {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    cout << "No" << endl;
+
+    return 0;
+}
