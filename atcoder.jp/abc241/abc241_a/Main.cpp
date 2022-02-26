5ac54fa5bc53b0aab89dc6e5b5142e645e476014
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-02-26 12:02:07 +0000

    [AC] abc241 abc241_a

---
+++ atcoder.jp/abc241/abc241_a/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    vector<int> a(10);
+    rep(i, 10) cin >> a[i];
+
+    int k = 0;
+    rep(i, 3)
+    {
+        k = a[k];
+    }
+    cout << k << endl;
+
+    return 0;
+}
