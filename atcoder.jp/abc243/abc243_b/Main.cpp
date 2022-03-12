2c26e2474c53dbdf55827c604caafc175710a811
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-03-12 12:11:03 +0000

    [AC] abc243 abc243_b

---
+++ atcoder.jp/abc243/abc243_b/Main.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cin >> b[i];
+
+    int ans1 = 0, ans2 = 0;
+
+    rep(i, n)
+    {
+        if (a[i] == b[i])
+            ans1++;
+    }
+
+    rep(i, n)
+    {
+        rep(j, n)
+        {
+            if (i == j)
+                continue;
+            if (a[i] == b[j])
+                ans2++;
+        }
+    }
+
+    cout << ans1 << endl << ans2 << endl;
+
+    return 0;
+}
