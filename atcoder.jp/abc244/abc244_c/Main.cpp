926044c78951bd9245140e6f0aa56524f3234a2a
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-03-20 12:17:09 +0000

    [AC] abc244 abc244_c

---
+++ atcoder.jp/abc244/abc244_c/Main.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> nums;
+
+    reps(i, 2 * n + 1) nums.push_back(i);
+
+    while (true)
+    {
+        int tak, aok;
+        cout << nums[nums.size() - 1] << endl;
+        nums.pop_back();
+
+        cin >> aok;
+        if (aok == 0)
+        {
+            return 0;
+        }
+
+        rep(i, nums.size())
+        {
+            if (nums[i] == aok)
+            {
+                nums.erase(nums.begin() + i);
+            }
+        }
+    }
+
+    return 0;
+}
