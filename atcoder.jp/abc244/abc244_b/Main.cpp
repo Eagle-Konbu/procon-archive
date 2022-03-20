654b01890f4bc6e401f15b5698be60dbc8e2ec9a
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-03-20 12:04:14 +0000

    [AC] abc244 abc244_b

---
+++ atcoder.jp/abc244/abc244_b/Main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n;
+    string t;
+    cin >> n >> t;
+
+    P pos = {0, 0};
+
+    P dir = {1, 0};
+
+    rep(i,n) {
+        if(t[i]=='S') {
+            pos.first += dir.first;
+            pos.second += dir.second;
+        } else {
+            if(dir.first == 1) {
+                dir.first = 0;
+                dir.second = -1;
+            } else if (dir.second == -1) {
+                dir.first = -1;
+                dir.second = 0;
+            } else if (dir.first == -1) {
+                dir.first = 0;
+                dir.second = 1;
+            } else {
+                dir.first = 1;
+                dir.second = 0;
+            }
+        }
+    }
+
+    cout << pos.first << " " << pos.second << endl;
+
+    return 0;
+}
