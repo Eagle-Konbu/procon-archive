6dc36b9a0cfb4072448dab59639613b42d7e6d21
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-04-10 16:58:13 +0000

    [AC] abc247 abc247_c

---
+++ atcoder.jp/abc247/abc247_c/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+string s(int n)
+{
+    if (n == 1)
+        return "1";
+    else
+        return s(n - 1) + " " + to_string(n) + " " + s(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << s(n) << endl;
+
+    return 0;
+}
