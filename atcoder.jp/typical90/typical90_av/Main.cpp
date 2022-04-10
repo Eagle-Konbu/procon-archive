0cfc795f34b3a78b933e1f0412cc79a2dbe0ba3f
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-04-10 23:02:05 +0000

    [AC] typical90 typical90_av

---
+++ atcoder.jp/typical90/typical90_av/Main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> scores;
+    rep(i, n)
+    {
+        int a, b;
+        cin >> a >> b;
+        scores.push_back(b);
+        scores.push_back(a - b);
+    }
+
+    SORT(scores);
+    reverse(all(scores));
+
+    ll ans = 0;
+    rep(i, k) ans += scores[i];
+
+    cout << ans << endl;
+
+    return 0;
+}
