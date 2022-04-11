82394449f414dc6f67a98110f5d1f6fbe197f95d
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-04-11 00:33:54 +0000

    [AC] typical90 typical90_cf

---
+++ atcoder.jp/typical90/typical90_cf/Main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    ll n;
+    string s;
+    cin >> n >> s;
+
+    ll ans = n * (n - 1) / 2, cnt = 0;
+    rep(i, n - 1)
+    {
+        if (s[i] == s[i + 1])
+        {
+            cnt++;
+        }
+        else
+        {
+            ans -= cnt * (cnt + 1) / 2;
+            cnt = 0;
+        }
+    }
+    ans -= cnt * (cnt + 1) / 2;
+    cout << ans << endl;
+
+    return 0;
+}
