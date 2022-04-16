c050331b639f19920b54f59ef3a4b1b904266c7f
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-04-16 09:20:01 +0000

    [AC] typical90 typical90_ax

---
+++ atcoder.jp/typical90/typical90_ax/Main.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+using mint = modint1000000007;
+
+int main()
+{
+    int n, l;
+    cin >> n >> l;
+
+    vector<mint> dp(n + 1, 0);
+    dp[0] = 1;
+    reps(i, n)
+    {
+        dp[i] += dp[i - 1];
+        if (i - l >= 0)
+            dp[i] += dp[i - l];
+    }
+
+    cout << dp[n].val() << endl;
+
+    return 0;
+}
