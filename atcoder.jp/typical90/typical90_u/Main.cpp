2ff632d1140a6ea3a908e98a5eec108cac150757
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-04-22 14:32:39 +0000

    [AC] typical90 typical90_u

---
+++ atcoder.jp/typical90/typical90_u/Main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    scc_graph g(n);
+
+    rep(i, m)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        g.add_edge(a, b);
+    }
+
+    auto scc = g.scc();
+
+    ll ans = 0;
+    for (auto points : scc)
+    {
+        ll cnt = points.size();
+        ans += cnt * (cnt - 1) / 2;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
