bc4ee458e730718e7e56c082e9ff169e85ed277e
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-05-24 12:03:58 +0000

    [AC] abc252 abc252_d

---
+++ atcoder.jp/abc252/abc252_d/Main.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+
+    ll max_a = *max_element(all(a));
+
+    vector<ll> cnts(max_a + 1);
+    rep(i, n) cnts[a[i]]++;
+
+    reps(x, max_a)
+    {
+        cnts[x] += cnts[x - 1];
+    }
+
+    ll ans = 0;
+    reps(middle, max_a - 1)
+    {
+        ans += cnts[middle - 1] * (cnts[middle] - cnts[middle - 1]) * (cnts[max_a] - cnts[middle]);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
