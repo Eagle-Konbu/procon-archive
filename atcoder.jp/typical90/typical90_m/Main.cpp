14cde438acf992e4f5c65528018c3b07a50f61dd
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-06-16 17:59:25 +0000

    [AC] typical90 typical90_m

---
+++ atcoder.jp/typical90/typical90_m/Main.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll factorial(ll n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+ll combination(ll a, ll b)
+{
+    return factorial(a) / (factorial(a - b) * factorial(b));
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+vector<ll> dijkstra(const WeightedGraph &g, ll s)
+{
+    vector<ll> dis(g.size(), LONG_LONG_MAX);
+    dis[s] = 0;
+    priority_queue<P, vector<P>, greater<P>> pq;
+
+    pq.push({0, s});
+    while (!pq.empty())
+    {
+        auto [current_dis, v] = pq.top();
+        pq.pop();
+        if (dis[v] < current_dis)
+            continue;
+        for (auto e : g[v])
+        {
+            if (chmin(dis[e.to], current_dis + e.weight))
+            {
+                pq.push({dis[e.to], e.to});
+            }
+        }
+    }
+    return dis;
+}
+
+int main()
+{
+    ll n, m;
+    cin >> n >> m;
+    WeightedGraph g(n);
+    rep(i, m)
+    {
+        ll a, b, c;
+        cin >> a >> b >> c;
+        a--;
+        b--;
+        g[a].push_back(Edge(b, c));
+        g[b].push_back(Edge(a, c));
+    }
+
+    auto dis = dijkstra(g, 0);
+    auto dis_n = dijkstra(g, n - 1);
+
+    rep(k, n) cout << dis[k] + dis_n[k] << endl;
+
+    return 0;
+}
