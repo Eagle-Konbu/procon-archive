63d242160d78cd3276641356b34ad55e2a90ce05
cl17 <a18.f7ft@g.chuo-u.ac.jp>
2022-06-18 10:27:40 +0000

    [AC] abc226 abc226_d

---
+++ atcoder.jp/abc226/abc226_d/Main.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+typedef long long ll;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define reps(i, n) for (ll i = 1; i <= (ll)(n); i++)
+#define rrep(i, n) for (ll i = (ll)n - 1; i >= 0; i--)
+#define rreps(i, n) for (ll i = (ll)n; i > 0; i--)
+#define SORT(x) sort(x.begin(), x.end())
+#define ERASE(x) x.erase(unique(x.begin(), x.end()), x.end())
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define all(x) x.begin(), x.end()
+
+template <typename T>
+bool chmax(T &a, const T &b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T &a, const T &b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll factorial(ll n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+ll combination(ll a, ll b)
+{
+    return factorial(a) / (factorial(a - b) * factorial(b));
+}
+
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+struct Edge
+{
+    ll to;
+    ll weight;
+    Edge(ll to, ll weight) : to(to), weight(weight) {}
+};
+typedef vector<vector<ll>> Graph;
+typedef vector<vector<Edge>> WeightedGraph;
+
+vector<ll> dijkstra(const WeightedGraph &g, ll s)
+{
+    vector<ll> dis(g.size(), LONG_LONG_MAX);
+    dis[s] = 0;
+    priority_queue<P, vector<P>, greater<P>> pq;
+
+    pq.push({0, s});
+    while (!pq.empty())
+    {
+        auto [current_dis, v] = pq.top();
+        pq.pop();
+        if (dis[v] < current_dis)
+            continue;
+        for (auto e : g[v])
+        {
+            if (chmin(dis[e.to], current_dis + e.weight))
+            {
+                pq.push({dis[e.to], e.to});
+            }
+        }
+    }
+    return dis;
+}
+
+vector<ll> topo_sort(const Graph &g)
+{
+    vector<ll> res;
+    ll n = g.size();
+    vector<ll> in_deg(n);
+
+    rep(i, n) for (auto vv : g[i]) in_deg[vv]++;
+
+    queue<ll> que;
+    rep(i, n) if (in_deg[i] == 0) que.push(i);
+
+    while (!que.empty())
+    {
+        ll v = que.front();
+        res.push_back(v);
+        que.pop();
+        for (auto vv : g[v])
+        {
+            in_deg[vv]--;
+            if (in_deg[vv] == 0)
+                que.push(vv);
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    ll n;
+    cin >> n;
+    vector<P> xy;
+    rep(i, n)
+    {
+        ll x, y;
+        cin >> x >> y;
+        xy.push_back({x, y});
+    }
+
+    set<P> st;
+    rep(i, n) rep(j, n)
+    {
+        if (i >= j)
+            continue;
+        ll g = gcd(xy[j].first - xy[i].first, xy[j].second - xy[i].second);
+        st.insert({(xy[j].first - xy[i].first) / g, (xy[j].second - xy[i].second) / g});
+    }
+
+    cout << st.size() * 2 << endl;
+
+    return 0;
+}
